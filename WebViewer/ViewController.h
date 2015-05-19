bfacc57e6f41aa63fc1bd8183eca96e28ed3d289
Loudent <troy.loudent\@autodesk.com>
2015-05-19 14:24:00 -0700

    It works

--- WebViewer/ViewController.h
+++ WebViewer/ViewController.h
@@ -9,7 +9,10 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
 
+@property (weak, nonatomic) IBOutlet UIButton *LoadURL;
+@property (weak, nonatomic) IBOutlet UITextField *URLText;
 
 @end
 
--- WebViewer/ViewController.m
+++ WebViewer/ViewController.m
@@ -17,11 +17,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //set a clear default
+    self.URLText.text=@"";
+    //get the last saved default
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *serverID = [defaults objectForKey:@"serverid"];
+    
+    self.URLText.text= serverID;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)LoadURL:(id)sender
+{
+    //Load The web server
+    NSString *url =@"http://";
+    url = [url stringByAppendingString:self.URLText.text];
+    url = [url stringByAppendingString:@"/mapserver2016/mobileviewer/index.html"];
+    NSMutableURLRequest * request =[NSMutableURLRequest requestWithURL:[NSURL URLWithString:url]];
+    [self.webView loadRequest:request];
+    
+    //Save the text;
+    NSString *serverID = [self.URLText text];
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    
+    [defaults setObject:serverID forKey:@"serverid"];
+    [defaults synchronize];
+    
+
+}
+
 
 @end
